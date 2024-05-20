f10e7e36483e324c0cb4bc8de08b6993b8e0bc68
Rioba-Ian <riobafelix@gmail.com>
2024-05-20 16:55:57 +0300

    create separate branch for the frontend

--- fiber-cloudinary-api/dtos/media_dto.go
+++
@@ -1,9 +0,0 @@
-package dtos
-
-import "github.com/gofiber/fiber/v2"
-
-type MediaDto struct {
-	StatusCode int        `json:"statusCode"`
-	Message    string     `json:"message"`
-	Data       *fiber.Map `json:"data"`
-}
